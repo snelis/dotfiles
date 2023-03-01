65dd1dfac8a084dae500b7dea6922506d2f87581
Niels Lensink <nielslensink@gmail.com>
2023-03-01 16:06:05 +0100

    added alacritty config

---
+++ dot_config/executable_tmux-theme-toggle.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+#
+# Toggle the current window (all panes) between light and dark themes.
+set -e
+
+default_window_style='fg=#d0cfcc,bg=#1E1E2E'
+light_window_style='fg=#171421,bg=#EFF1F5'
+current_window_style=$(tmux show -Av window-style)
+
+case $current_window_style in
+    $default_window_style|'default')
+        # Change to the alternate window style.
+        tmux set window-style $light_window_style
+        tmux set pane-active-border-style 'fg=#1C71D8,bg=#EFF1F5'
+        tmux set pane-border-style 'fg=#999999,bg=#EFF1F5' 
+        echo "let &background='light'" > ~/.vimrc_background
+        tmux send-keys F7
+        ;;
+    *)
+        # Change back to the default window style.
+        tmux set window-style $default_window_style
+        tmux set pane-active-border-style 'fg=#1C71D8,bg=#1E1E2E'
+        tmux set pane-border-style 'fg=#ffffff,bg=#1E1E2E' 
+        echo "let &background='dark'" > ~/.vimrc_background
+        tmux send-keys F7
+        ;;
+esac
