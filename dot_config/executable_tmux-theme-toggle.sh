f82df0c5734133bd7759d026e73d0e2b7a6e7878
Niels <nielslensink@gmail.com>
2023-09-06 12:41:33 +0200

    Send toggle light and dark to all panes running vim

--- dot_config/executable_tmux-theme-toggle.sh
+++ dot_config/executable_tmux-theme-toggle.sh
@@ -14,7 +14,7 @@ case $current_window_style in
         tmux set pane-active-border-style 'fg=#1C71D8,bg=#EFF1F5'
         tmux set pane-border-style 'fg=#999999,bg=#EFF1F5' 
         echo "let &background='light'" > ~/.vimrc_background
-        tmux send-keys F7
+        tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=light" ENTER
         ;;
     *)
         # Change back to the default window style.
@@ -22,6 +22,6 @@ case $current_window_style in
         tmux set pane-active-border-style 'fg=#1C71D8,bg=#1E1E2E'
         tmux set pane-border-style 'fg=#ffffff,bg=#1E1E2E' 
         echo "let &background='dark'" > ~/.vimrc_background
-        tmux send-keys F7
+        tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=dark" ENTER
         ;;
 esac
