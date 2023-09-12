d648e52cff35f4c4c3bdd0b2fd5f8464fee38619
Niels <nielslensink@gmail.com>
2023-09-12 14:27:01 +0200

    Updated py3status and tmux

--- dot_config/executable_tmux-theme-toggle.sh
+++ dot_config/executable_tmux-theme-toggle.sh
@@ -10,18 +10,20 @@ current_window_style=$(tmux show -Av window-style)
 case $current_window_style in
     $default_window_style|'default')
         # Change to the alternate window style.
-        tmux set window-style $light_window_style
-        tmux set pane-active-border-style 'fg=#1C71D8,bg=#EFF1F5'
-        tmux set pane-border-style 'fg=#999999,bg=#EFF1F5' 
-        echo "let &background='light'" > ~/.vimrc_background
+        tmux list-windows -a -F "#{window_id}" | while read -r window_id; do
+          tmux setw -t "$window_id" window-style $light_window_style
+          tmux setw -t "$window_id" pane-active-border-style 'fg=#1C71D8,bg=#EFF1F5'
+          tmux setw -t "$window_id" pane-border-style 'fg=#999999,bg=#EFF1F5' 
+        done
         tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=light" ENTER
         ;;
     *)
         # Change back to the default window style.
-        tmux set window-style $default_window_style
-        tmux set pane-active-border-style 'fg=#1C71D8,bg=#1E1E2E'
-        tmux set pane-border-style 'fg=#ffffff,bg=#1E1E2E' 
-        echo "let &background='dark'" > ~/.vimrc_background
+        tmux list-windows -a -F "#{window_id}" | while read -r window_id; do
+          tmux setw -t "$window_id" window-style $default_window_style
+          tmux setw -t "$window_id" pane-active-border-style 'fg=#1C71D8,bg=#1E1E2E'
+          tmux setw -t "$window_id" pane-border-style 'fg=#ffffff,bg=#1E1E2E' 
+        done
         tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=dark" ENTER
         ;;
 esac
