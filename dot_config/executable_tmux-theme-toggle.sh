c586d9b20d7549eff8b55c7eed598e2f56565fcd
Niels <nielslensink@gmail.com>
2023-09-13 19:18:30 +0200

    implement another toggle theme switching for alacritty

--- dot_config/executable_tmux-theme-toggle.sh
+++ dot_config/executable_tmux-theme-toggle.sh
@@ -3,27 +3,21 @@
 # Toggle the current window (all panes) between light and dark themes.
 set -e
 
-default_window_style='fg=#d0cfcc,bg=#1E1E2E'
-light_window_style='fg=#171421,bg=#EFF1F5'
-current_window_style=$(tmux show -Av window-style)
+LIGHTTHEME=Mocha.light.yml
+DARKTHEME=Mocha.yml
+ALACRITTYCONF=~/.config/alacritty/alacritty.yml
 
-case $current_window_style in
-    $default_window_style|'default')
-        # Change to the alternate window style.
-        tmux list-windows -a -F "#{window_id}" | while read -r window_id; do
-          tmux setw -t "$window_id" window-style $light_window_style
-          tmux setw -t "$window_id" pane-active-border-style 'fg=#1C71D8,bg=#EFF1F5'
-          tmux setw -t "$window_id" pane-border-style 'fg=#999999,bg=#EFF1F5' 
-        done
-        tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=light" ENTER
-        ;;
-    *)
-        # Change back to the default window style.
-        tmux list-windows -a -F "#{window_id}" | while read -r window_id; do
-          tmux setw -t "$window_id" window-style $default_window_style
-          tmux setw -t "$window_id" pane-active-border-style 'fg=#1C71D8,bg=#1E1E2E'
-          tmux setw -t "$window_id" pane-border-style 'fg=#ffffff,bg=#1E1E2E' 
-        done
-        tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=dark" ENTER
-        ;;
-esac
+# Check if the current theme is dark
+if grep -q "$DARKTHEME" "$ALACRITTYCONF"; then
+    # Replace the dark theme with the light one
+    sed -i 's/'"$DARKTHEME"'/'"$LIGHTTHEME"'/' "$ALACRITTYCONF"
+    # Debug: Print a message to confirm the theme change
+    # Find and switch Vim panes to the corresponding background setting
+    tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=light" ENTER
+else
+    # Replace the light theme with the dark one
+    sed -i "s/$LIGHTTHEME/$DARKTHEME/" ~/.config/alacritty/alacritty.yml
+    # Debug: Print a message to confirm the theme change
+    # Find and switch Vim panes to the corresponding background setting
+    tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=dark" ENTER
+fi
