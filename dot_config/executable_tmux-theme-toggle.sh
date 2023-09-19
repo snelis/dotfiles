4715600e875188d9b1b0c64ef38e72538c4e8a55
Niels <nielslensink@gmail.com>
2023-09-19 13:49:51 +0200

    Updated theme method

--- dot_config/executable_tmux-theme-toggle.sh
+++ dot_config/executable_tmux-theme-toggle.sh
@@ -3,22 +3,24 @@
 # Toggle the current window (all panes) between light and dark themes.
 set -e
 
-LIGHTTHEME=Alabaster.yml
-DARKTHEME=Tender.yml
-DARKTHEME=github_dark_default.yml
-DARKTHEME=iTerm-Default.yml
+LIGHTTHEME=Alabaster
+DARKTHEME=Tender
+DARKTHEME=github_dark_default
+DARKTHEME=iTerm-Default
 ALACRITTYCONF=~/.config/alacritty/alacritty.yml
 
 # Check if the current theme is dark
-if grep -q "$DARKTHEME" "$ALACRITTYCONF"; then
+if grep -q "theme: $DARKTHEME" "$ALACRITTYCONF"; then
     # Replace the dark theme with the light one
-    sed -i 's/'"$DARKTHEME"'/'"$LIGHTTHEME"'/' "$ALACRITTYCONF"
+    # sed -i 's/'"$DARKTHEME"'/'"$LIGHTTHEME"'/' "$ALACRITTYCONF"
+    alacritty-themes $LIGHTTHEME 1>/dev/null
     # Debug: Print a message to confirm the theme change
     # Find and switch Vim panes to the corresponding background setting
     tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=light" ENTER
 else
     # Replace the light theme with the dark one
-    sed -i "s/$LIGHTTHEME/$DARKTHEME/" ~/.config/alacritty/alacritty.yml
+    # sed -i "s/$LIGHTTHEME/$DARKTHEME/" ~/.config/alacritty/alacritty.yml
+    alacritty-themes $DARKTHEME 1>/dev/null
     # Debug: Print a message to confirm the theme change
     # Find and switch Vim panes to the corresponding background setting
     tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep vim | cut -d ' ' -f 1 | xargs -I PANE tmux send-keys -t PANE ESCAPE ":set background=dark" ENTER
