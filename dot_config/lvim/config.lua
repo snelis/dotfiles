b4662b6007fab57850fbe164c08edd97e60c92ac
Niels <nielslensink@gmail.com>
2023-09-21 11:19:01 +0200

    updated i3 config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -140,14 +140,26 @@ lvim.plugins = {
 }
 
 -- -- Enable copilot
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
+--
 -- table.insert(lvim.plugins, {
---   "zbirenbaum/copilot-cmp",
---   event = "InsertEnter",
---   dependencies = { "zbirenbaum/copilot.lua" },
---   config = function()
---     vim.defer_fn(function()
---       require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
---       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
---     end, 100)
---   end,
+--   "github/copilot.vim",
+--   -- event = "InsertEnter",
+--   -- dependencies = { "zbirenbaum/copilot.lua" },
+--   -- config = function()
+--   --   vim.defer_fn(function()
+--   --     require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+--   --     require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+--   --   end, 100)
+--   -- end,
 -- })
